ecdee60306734d9404552ab7e3c3c9fffb5f6368
Yuval Deutscher <yuval@deut.sh>
2020-09-09 18:11:15 +0300

    remove dynamic resolving of opener

--- src/gtfs.rs
+++ src/gtfs.rs
@@ -4,6 +4,8 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+mod opener;
+
 use crate::HaError;
 use crate::JSON;
 use chrono::{Datelike, Duration, NaiveDate};
@@ -299,21 +301,6 @@ impl Train {
     /// Iterate over the train schedule days
     pub fn dates(&self) -> impl Iterator<Item = &NaiveDate> {
         self.dates.iter()
-    }
-}
-
-enum FileOpener<'p, R: Read + Seek> {
-    PathFileOpener(&'p Path),
-    ZipFileOpener(ZipArchive<R>),
-}
-
-impl<'p, R: Read + Seek> FileOpener<'p, R> {
-    fn open<'a>(&'a mut self, name: &str) -> Result<Box<dyn Read + 'a>, Box<dyn Error>> {
-        let result: Box<dyn Read> = match self {
-            FileOpener::PathFileOpener(path) => Box::new(File::open(path.join(name))?),
-            FileOpener::ZipFileOpener(zip) => Box::new(zip.by_name(name)?),
-        };
-        Ok(result)
     }
 }
 
@@ -714,9 +701,7 @@ impl RailroadData {
         Ok(stations)
     }
 
-    fn load_gtfs<'p, R: Read + Seek>(
-        mut opener: FileOpener<'p, R>,
-    ) -> Result<Self, Box<dyn Error>> {
+    fn load_gtfs<T: for<'a> opener::FileOpener<'a>>(mut opener: T) -> Result<Self, Box<dyn Error>> {
         let irw_id = Self::parse_agency(opener.open("agency.txt")?)?;
         let irw_routes = Self::parse_routes(opener.open("routes.txt")?, irw_id)?;
         let services = Self::parse_calendar(opener.open("calendar.txt")?)?;
@@ -729,7 +714,7 @@ impl RailroadData {
 
     /// Loads a GTFS file database from a directory containing GTFS text files.
     pub fn from_gtfs_directory(root: &Path) -> Result<Self, Box<dyn Error>> {
-        let opener = FileOpener::PathFileOpener::<File>(root);
+        let opener = opener::PathFileOpener::new(root);
         Self::load_gtfs(opener)
     }
 
@@ -737,7 +722,7 @@ impl RailroadData {
     pub fn from_gtfs_zip(root: &Path) -> Result<Self, Box<dyn Error>> {
         let file = File::open(root)?;
         let zip = ZipArchive::new(file)?;
-        let opener = FileOpener::ZipFileOpener(zip);
+        let opener = opener::ZipFileOpener::new(zip);
         Self::load_gtfs(opener)
     }
 }
